2974ff563b20942e39a7c6d467fc3bffd7cfa3cf
Lulu-Luchar <chillhouette@gmail.com>
2019-08-24 21:01:21 -0700

    Added DebounceTime

--- src/app/show-search/show-search.component.ts
+++ src/app/show-search/show-search.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ShowsService } from '../shows/shows.service';
+import { debounceTime } from 'rxjs/operators';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-show-search',
@@ -8,13 +10,16 @@ import { ShowsService } from '../shows/shows.service';
   styleUrls: ['./show-search.component.css']
 })
 export class ShowSearchComponent implements OnInit {
+  @Output() searchEvent= new EventEmitter;
 
   search = new FormControl('',[Validators.minLength(3)]);
 
   constructor(private showsService: ShowsService) { }
 
   ngOnInit() {
-    this.search.valueChanges.subscribe((searchValue: string)=> {
+    this.search.valueChanges
+    .pipe(debounceTime(1000))
+    .subscribe((searchValue: string)=> {
       if(!this.search.invalid) {
         const userInput = searchValue;
         this.showsService.getShowData(userInput.length>1 ? userInput[1]: undefined).subscribe(data=>console.log(data));
